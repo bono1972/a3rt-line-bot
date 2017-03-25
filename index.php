3107c8a3833be06b6c522ddd99629b261255b779
Tetsuya Sakai <t.sakai.1972@gmail.ocm>
2017-03-26 03:15:44 +0900

    modified

--- index.php
+++ index.php
@@ -29,8 +29,15 @@ foreach ($events as $event) {
     continue;
   }
   $getText = $event->gettext();
-  $reply = getTalk($getText);
-  $bot->replyText($event->getReplyToken(), $getText);
+  $message = getTalk($getText);
+	replyTextMessage($bot, $event->getReplyToken(),$message);
+}
+
+function replyTextMessage($bot, $replyToken, $text) {
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
+  if (!$response->isSucceeded()) {
+    error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+  }
 }
 
 function getTalk ($text) {
